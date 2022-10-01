a7a3fd1b0f52c193b7888c82f2194037ba0be057
Aviraj Khare <avirajkhare00@gmail.com>
2022-10-01 08:47:12 +0530

    [Aviraj] Insert tests inside describe function

--- tests/text-sanitization.test.js
+++ tests/text-sanitization.test.js
@@ -1,13 +1,18 @@
 import textSanitization from '../src/utils/text-sanitization/index.js';
 
-test('replace newline with whitespace', () => {
-    expect(textSanitization("My name is Aviraj Khare.\n")).toBe("My name is Aviraj Khare. ");
-});
-
-test('text is not changed if no newline', () => {
-    expect(textSanitization("My name is Aviraj Khare.")).toBe("My name is Aviraj Khare.");
-});
-
-test('text is not changed if no text', () => {
-    expect(textSanitization("")).toBe("");
-});
+describe('text-sanitization test suite', () => {
+    test('replace newline with whitespace', () => {
+        expect(textSanitization("My name is Aviraj Khare.\n")).toBe("My name is Aviraj Khare. ");
+    });
+    
+    test('text is not changed if no newline', () => {
+        expect(textSanitization("My name is Aviraj Khare.")).toBe("My name is Aviraj Khare.");
+    });
+    
+    test('text is not changed if no text', () => {
+        expect(textSanitization("")).toBe("");
+    });
+    test('textSanitization should be a string', () => {
+        expect(typeof textSanitization("")).toBe('string');
+    })
+})
