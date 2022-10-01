1797841b97853f999b79084120fab5e021630e82
Aviraj Khare <avirajkhare00@gmail.com>
2022-10-01 11:57:05 +0530

    [Aviraj] Added test to check headers

--- tests/text-to-speech-convertor.test.js
+++ tests/text-to-speech-convertor.test.js
@@ -4,10 +4,6 @@ import { executablePath } from "puppeteer";
 import textToSpeechConvertor from "../src/utils/text-to-speech-convertor";
 
 describe('POST', () => {
-    // increasing timeout due to issue
-    // beforeEach(() => {
-    //     jest.setTimeout(600000);
-    // });
     it('returns data when ibm synthesize api is called', done => {
         // This sets the mock adapter on the default instance
         var mock = new MockAdapter(axios);
@@ -20,6 +16,23 @@ describe('POST', () => {
         })
         .then(function(data){
             expect(data.status).toBe(200);
+        });
+        done();
+    });
+
+    it('receives correct headers when the api is called', done => {
+        var mock = new MockAdapter(axios);
+        mock.onPost("/v1/synthesize/").reply(200);
+        axios({
+            url: '/v1/synthesize/',
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "audio/wav"
+            }
+        })
+        .then(function(response){
+            expect(response.config.headers).toStrictEqual({"Accept": "audio/wav", "Content-Type": "application/json"});
         });
         done();
     });
