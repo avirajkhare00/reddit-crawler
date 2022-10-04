e25a8c076fffe554e06e3fa4e66bf97ebbffd46d
Aviraj Khare <avirajkhare00@gmail.com>
2022-10-04 16:27:48 +0530

    [Aviraj] Removed async option from downloadAudioFromText

--- src/utils/bash-file-downloader/index.js
+++ src/utils/bash-file-downloader/index.js
@@ -2,11 +2,8 @@ import shell from 'shelljs';
 
 async function downloadAudioFromText(apiKey, text, fileName, url, res) {
   const jsonData = JSON.stringify({ text });
-  const child = shell.exec(`curl -X POST -u 'apikey:${apiKey}' --header 'Content-Type: application/json' --header 'Accept: audio/wav' --data '${jsonData}' --output public/output/${fileName} '${url}?voice=en-US_MichaelV3Voice'`, { async: true });
-  child.stdout.on('data', async (data) => {
-    console.log(data);
-    await res.json({ fileName });
-  });
+  shell.exec(`curl -X POST -u 'apikey:${apiKey}' --header 'Content-Type: application/json' --header 'Accept: audio/wav' --data '${jsonData}' --output public/output/${fileName} '${url}?voice=en-US_MichaelV3Voice'`);
+  res.json({ fileName });
 }
 
 export default downloadAudioFromText;
