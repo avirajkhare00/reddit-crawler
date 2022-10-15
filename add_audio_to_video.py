8d807361ae29aae95a607d0652c352f7f23eaef1
Muskan Marwah <marwahmuskan330@gmail.com>
2022-10-15 18:45:36 +0530

    [Muskan] Fixed imports

--- add_audio_to_video.py
+++ add_audio_to_video.py
@@ -1,4 +1,7 @@
-from moviepy.editor import *
+from moviepy.video.VideoClip import TextClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
+
 
 screensize = (720,460)
 
@@ -11,3 +14,4 @@ videoclip = videoclip.set_audio(AudioFileClip('1665833609012.mp3'))
 videoclip = CompositeVideoClip([videoclip], size=screensize)
 
 videoclip.write_videofile("new_filename.mp4", fps=30, codec='libx264', audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
+
